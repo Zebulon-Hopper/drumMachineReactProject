99e539fc11008bcd1bc6200913f9b9ce24b6d2a9
Zebulon Hopper <zebulonhopper@gmail.com>
2022-10-25 13:25:30 -0500

    load drum  repo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react';
 
-function App() {
+
+
+// Objective: Fulfill the below user stories and get all of the tests to pass. Give it your own personal style. You can use any mix of HTML, JavaScript, CSS, Bootstrap, SASS, React, Redux, and jQuery to complete this project. You should use a frontend framework (like React for example) because this section is about learning frontend frameworks. Additional technologies not listed above are not recommended and using them is at your own risk. We are looking at supporting other frontend frameworks like Angular and Vue, but they are not currently supported. We will accept and try to fix all issue reports that use the suggested technology stack for this project. Happy coding! User Story #1: I should be able to see an outer container with a corresponding id="drum-machine" that contains all other elements. User Story #2: Within #drum-machine I can see an element with a corresponding id="display". User Story #3: Within #drum-machine I can see 9 clickable drum pad elements, each with a class name of drum-pad, a unique id that describes the audio clip the drum pad will be set up to trigger, and an inner text that corresponds to one of the following keys on the keyboard: Q, W, E, A, S, D, Z, X, C. The drum pads MUST be in this order. User Story #4: Within each .drum-pad, there should be an HTML5 audio element which has a src attribute pointing to an audio clip, a class name of clip, and an id corresponding to the inner text of its parent .drum-pad (e.g. id="Q", id="W", id="E" etc.). User Story #5: When I click on a .drum-pad element, the audio clip contained in its child audio element should be triggered. User Story #6: When I press the trigger key associated with each .drum-pad, the audio clip contained in its child audio element should be triggered (e.g. pressing the Q key should trigger the drum pad which contains the string "Q", pressing the W key should trigger the drum pad which contains the string "W", etc.). User Story #7: When a .drum-pad is triggered, a string describing the associated audio clip is displayed as the inner text of the #display element (each string must be unique). Once you're done, submit the URL to your working project with all its tests passing.
+
+const App=()=>{
+  const[display,setDisplay]=useState('')
+const handleSound=(event)=>{
+  if(!event.code){
+    console.log(event.target.id)
+    setDisplay(event.target.id)
+    let sound=require(`./Audio/${event.target.id}.wav`)
+    let audio = new Audio(sound)
+    audio.play() }
+    else{
+      console.log(event.code)
+      setDisplay(event.code)
+      let sound=require(`./Audio/${event.code}.wav`)
+      let audio = new Audio(sound)
+      audio.play() }
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div id='drum-machine'>{/*User Story #1*/}
+    <div id='display-container'>
+      <h1 id='display'>{display}</h1>{/*User Story #2*/}
+      </div>
+      
+  <button id='keyboard' onKeyPress={handleSound}>Click to use keyboard</button>
+  <div id='drum-pad-container'>
+  <button id='KeyQ'  className='drum-pad'  onClick={handleSound} >Q</button>
+  <button id='KeyW'  className='drum-pad'  onClick={handleSound} >W</button>
+  <button id='KeyE'  className='drum-pad'  onClick={handleSound} >E</button>
+  <button id='KeyA'  className='drum-pad'  onClick={handleSound} >A</button>
+  <button id='KeyS'  className='drum-pad'  onClick={handleSound} >S</button>
+  <button id='KeyD'  className='drum-pad'  onClick={handleSound} >D</button>
+  <button id='KeyZ'  className='drum-pad'  onClick={handleSound} >Z</button>
+  <button id='KeyX'  className='drum-pad'  onClick={handleSound} >X</button>
+  <button id='KeyC'  className='drum-pad'  onClick={handleSound} >C</button>
+  </div>
+  </div>
+
   );
 }
 
